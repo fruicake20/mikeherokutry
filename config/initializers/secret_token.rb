c6857af6fbf0bfecfe52d72f8d0c0fdd084d91e3
Michael Agtual <mike_tech@mail.eyp.ph>
2017-08-23 15:38:59 +0800

    delete

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SimpleCms::Application.config.secret_token = '0f9f94f57402bc410cc51892bfc525962c1f6ff6086532ad14c0f79af699a77991de79061c880607f8745fbd0e6eefda8cecd39d8dbf5aa185e38fe7e790cd75'
